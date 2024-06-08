a349569e76678b6f9aec0f7f5450c0f0347c4dc4
Omar-WD <129740652+Omar-WD@users.noreply.github.com>
2024-06-09 00:57:33 +0200

    adding some details

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
-const express = require('express');
-require('dotenv').config();
-const cors=require('cors');
-const cookieParser = require('cookie-parser');
-const orderRouter=require('./routers/orders');
-const productRouter=require('./routers/products');
-const userRouter = require('./routers/users');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const port=process.env.PORT || 5000;
-require('./db');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const orderRouter = require("./routers/orders");
+const productRouter = require("./routers/products");
+const userRouter = require("./routers/users");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const port = process.env.PORT || 5000;
+require("./db");
 
 const app = express();
+app.use(express.static(path.join(__dirname, "client", "dist")))
 
-
-app.use(cors
-    (
-        {
-            origin: ['http://localhost:5173','https://e-commerce-kbkd.onrender.com'],
-            credentials: true,
-        }
-)
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://e-commerce-kbkd.onrender.com"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
 );
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/orders", orderRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/api/payment/create-payment-intent", async (req, res) => {
+  const { amount } = req.body;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount,
+    currency: "eur",
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html" ))
+  }) 
 
 
-app.use('/api/orders',orderRouter);
-app.use('/api/products',productRouter);
-app.use('/api/users',userRouter);
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    });
-
-app.post('/api/payment/create-payment-intent', async (req, res) => {
-    const { amount } = req.body;
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount,
-        currency: 'eur',
-        });
-    res.send({
-        clientSecret: paymentIntent.client_secret,
-        });
-    }
-);
-
+app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server is running on port http://localhost:${port}`);
-    });
+  console.log(`Server is running on port http://localhost:${port}`);
+});
