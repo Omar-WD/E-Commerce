5a946936bf504859ed4b1f8b9b2264d5ed42ac02
Omar Al Zoubi <129740652+Omar-WD@users.noreply.github.com>
2024-06-15 18:04:59 +0200

    Update ProductView.jsx

--- client/src/components/ProductView.jsx
+++ client/src/components/ProductView.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom"; 
 import { useContext, useState } from "react";
 import { OrderContext } from "../context/OrderProvider";
 import { ProductContext } from "../context/ProductProvider";
