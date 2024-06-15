23cf93821795fe2dae754f55a61b9daa70400d43
Omar-WD <129740652+Omar-WD@users.noreply.github.com>
2024-06-15 16:26:37 +0200

    adding more categories

--- client/src/axiosClient.js
+++ client/src/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-    // baseURL: 'https://e-commerce-kbkd.onrender.com/api',
-    baseURL: 'http://localhost:4500/api',
+    baseURL: 'https://e-commerce-kbkd.onrender.com/api',
+    // baseURL: 'http://localhost:4500/api',
     withCredentials: true,
     });
