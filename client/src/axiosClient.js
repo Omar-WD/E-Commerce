6f6a0243c4be50e7e881fb041a81e5e60ffbba68
Omar Al Zoubi <129740652+Omar-WD@users.noreply.github.com>
2024-06-19 00:30:03 +0200

    Update axiosClient.js

--- client/src/axiosClient.js
+++ client/src/axiosClient.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-    baseURL: 'https://e-commerce-1-6kw7.onrender.com/api',
-    // baseURL: 'http://localhost:4500/api',
+    baseURL: 'https://b.radwantravel.com/api',
     withCredentials: true,
     });
