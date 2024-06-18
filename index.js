891a602a79d63aa57b164bff5894739a318d38e8
Omar Al Zoubi <129740652+Omar-WD@users.noreply.github.com>
2024-06-18 21:58:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,9 @@ const path = require("path");
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-useNewUrlParser: true,
-useUnifiedTopology: true
-})
-.then(() => console.log('DB Connected'))
-.catch(err => console.error("Error connecting to MongoDB", err));
+mongoose.connect(process.env.MONGO_URI, {})
+  .then(() => console.log('DB Connected'))
+  .catch(err => console.error("Error connecting to MongoDB", err));
 
 const app = express();
 
